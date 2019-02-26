69be7f0af2a2962e4648920b7f6880278f27411a
PhilippMoor <philipp.moor@gmx.ch>
2019-02-26 17:17:38 +0100

    changes

--- Gauss_Quadratur.m
+++ Gauss_Quadratur.m
@@ -1,5 +1,17 @@
 clear all;close all;clc;
 
-for k=1:10
-    disp(k)
-end
+% dimension n = #number of nodes in one coordinate
+n = 2;
+% f = function to be integrated over the ->square<-
+
+% load matrices with nodes and weights
+nodes = load("nodes.mat");
+weights = load("weights.mat");
+
+x = nodes(n,1:n)
+y = x
+weights_x = weights(n,1:n)
+weights_y = weights_x
+
+
+
