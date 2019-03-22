4de52dceaa152de9be7c7e1b36bb138b94d617d4
Buuch1196 <jonas.guleruzh.ch>
2019-03-22 15:12:39 +0100

    adding rho transformation to square

--- Gauss_Quadrature.m
+++ Gauss_Quadrature.m
@@ -4,13 +4,13 @@ clear all;close all;clc;
 n = 2;
 % f = function to be integrated over the ->square<-
 
-% load matrices with nodes and weights
+% load structure with nodes and weights
 struct = load("nodes.mat");
-
 
 x = struct.nodes(n+1,1:n+1);
 y = struct.nodes(n,1:n);
 weights_x = struct.weights(n+1,1:n+1);
 weights_y = struct.weights(n,1:n);
 
-
+% definition of rho
+rho=@(a,b)[a,a*b];
