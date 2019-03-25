f984a80e6987b6f0503802e659dd11adc7ea5e26
Buuch1196 <jonas.guleruzh.ch>
2019-03-25 20:42:44 +0100

    added transformation to square and Quadrature sumation

--- Gauss_Quadrature.m
+++ Gauss_Quadrature.m
@@ -1,7 +1,7 @@
 clear all;close all;clc;
 
 % dimension n = #number of nodes in one coordinate
-n = 2;
+n = 10;
 % f = function to be integrated over the ->square<-
 
 % load structure with nodes and weights
@@ -13,4 +13,26 @@ weights_x = struct.weights(n+1,1:n+1);
 weights_y = struct.weights(n,1:n);
 
 % definition of rho
-rho=@(a,b)[a,a*b];
+rho_1=@(a,b)a;
+rho_2=@(a,b)a*b;
+
+%definition of f
+f=@(x,y) x.^2+y.^2;
+
+% function after transformation
+f_rho=@(a,b) f(rho_1(a,b),rho_2(a,b))*a;
+
+% Computing the Quadrature
+sum=0;
+for i=1:n+1
+    sum_y=0;
+    for j=1:n
+        sum_y=sum_y+(weights_x(i)*weights_y(j)*f_rho(x(i),y(j)));
+    end
+    sum=sum+sum_y;
+end
+disp(sum)
+
+
+
+
