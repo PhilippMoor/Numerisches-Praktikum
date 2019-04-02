4dff12853d525c8669907f0f8e5d24e386bc0772
PhilippMoor <philipp.moor@gmx.ch>
2019-04-02 15:30:30 +0200

    Update Gauss_Quadrature.m

--- Gauss_Quadrature.m
+++ Gauss_Quadrature.m
@@ -5,7 +5,10 @@ n = 2;
 % f = function to be integrated over the ->square<-
 
 % load matrices with nodes and weights
+nodes = load("nodes.mat")
 weights = load("weights.mat");
 
+x = nodes.nodes(n,1:n)
 y = x
+weights_x = weights.weights(n,1:n)
 weights_y = weights_x
