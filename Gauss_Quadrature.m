4a1f037cfd070c3ff7aac72aec87f8e5a4a36c6a
PhilippMoor <philipp.moor@gmx.ch>
2019-04-11 13:43:18 +0200

    more testing

--- Gauss_Quadrature.m
+++ Gauss_Quadrature.m
@@ -15,7 +15,7 @@ sum=0;
 for i=1:n+1
     sum_y=0;
     for j=1:n
-        nodes=[x(i),y(j)];
+        nodes=[x(i);y(j)];
         sum_y=sum_y+(weights_x(i)*weights_y(j)*f(nodes));
     end
     sum=sum+sum_y;
