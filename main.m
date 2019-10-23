d71afee11fe8e402ddc8b7c540480280c9a27054
Buuch1196 <jonas.guleruzh.ch>
2019-10-23 15:27:45 +0200

    adding testing for function h

--- main.m
+++ main.m
@@ -2,18 +2,20 @@
 clear all; close all; clc;
 
 A=[1;2;0];B=[-1;0;3];C=[4;1;1];
-type=1;
 
-n=10;
+max=20;
+error=zeros(3,max);
 
-% Computing transformation matrix on unit triangle
-m_tau=make_m_tau_mat(A,B,C,type);
 
 % defining integrand over triangle
-f=@(a) x^.2+3*x*y+y;
+f=@(x) 1/sqrt((x(1)-A(1))^2+(x(2)-A(2))^2);
 
-% transforming to integrand over unit square
-g=@(x) f(chi(A,B,C,m_tau,type,rho(x)))*sqrt(det(m_tau'*m_tau))*x(1);
-
-Gauss_Quadrature(n,g)
+for type=1:3
+    
+    for n=1:10
+        m_tau=make_m_tau_mat(A,B,C,type);
+        g=@(x) f(chi(A,B,C,m_tau,type,rho(x)))*sqrt(det(m_tau'*m_tau))*x(1);
+        Gauss_Quadrature(n,g)
+    end
+end
 
---
+++ testing_f3.m
@@ -0,0 +1,46 @@
+clear all; close all; clc;
+
+A=[1;2;0];B=[-1;0;3];C=[4;1;1];
+P=A;
+n=20;
+
+% defining f
+f=@(x) log(sqrt((x(1)-P(1))^2+(x(2)-P(2))^2));
+
+type=1;
+m_tau=make_m_tau_mat(A,B,C,type);
+g=@(x) f(chi(A,B,C,m_tau,type,rho(x)))*sqrt(det(m_tau'*m_tau))*x(1);
+I=Gauss_Quadrature(20,g)
+rel_error_1=zeros(n,1);value=zeros(n,1);
+for k=1:n
+    value(k)=Gauss_Quadrature(k,g);
+    rel_error_1(k)=abs(I-value(k));
+end
+
+type=2;
+m_tau=make_m_tau_mat(A,B,C,type);
+g=@(x) f(chi(A,B,C,m_tau,type,rho(x)))*sqrt(det(m_tau'*m_tau))*x(1);
+I=Gauss_Quadrature(20,g)
+rel_error_2=zeros(n,1);value=zeros(n,1);
+for k=1:n
+    value(k)=Gauss_Quadrature(k,g);
+    rel_error_2(k)=abs(I-value(k));
+end
+
+type=3;
+m_tau=make_m_tau_mat(A,B,C,type);
+g=@(x) f(chi(A,B,C,m_tau,type,rho(x)))*sqrt(det(m_tau'*m_tau))*x(1);
+I=Gauss_Quadrature(20,g)
+rel_error_3=zeros(n,1);value=zeros(n,1);
+for k=1:n
+    value(k)=Gauss_Quadrature(k,g);
+    rel_error_3(k)=abs(I-value(k));
+end
+
+
+h = figure();
+semilogy(1:n,rel_error_1,'*',1:n,rel_error_2,'*',1:n,rel_error_3,'*')
+hold on
+legend('type A','type B','type C')
+xlabel('n');
+grid on
