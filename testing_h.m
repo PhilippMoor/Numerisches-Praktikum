98da6df10f7c52de2650882f45f26907b97c686f
Buuch1196 <jonas.guleruzh.ch>
2019-11-06 09:39:43 +0100

    renaming for testing fct h

--- testing_h.m
+++ testing_h.m
@@ -19,7 +19,7 @@ I=Gauss_Quadrature(40,g_1)+Gauss_Quadrature(40,g_2)
 rel_error_1=zeros(n,1);value=zeros(n,1);step_error=zeros(n-1,1);
 for k=1:n
     value(k)=Gauss_Quadrature(k,g_1)+Gauss_Quadrature(k,g_2);
-    rel_error_1(k)=abs(I-value(k));
+    rel_error_1(k)=abs((I-value(k))/I);
     if k>1
         step_error(k-1)=abs(value(k)-value(k-1));
     end
@@ -34,44 +34,5 @@ semilogy(1:n,rel_error_1,'blue*',1:n-1, step_error,'redo')
 legend('relative error','step error')
 xlabel('n');
 grid on
-
-
-%%
-
-clear all; clc;
-
-A=[1;2;0];B=[-1;0;3];C=[4;1;1];
-type=1;
-
-% Computing transformation matrix on unit triangle
-m_tau=make_m_tau_mat(A,B,C,type);
-
-% defining integrand over triangle
-f=@(x) exp(x(1))*log(norm([x(1);x(2)]-A(1:2,1)));
-
-% transforming to integrand over unit square
-g=@(x) f(chi(A,B,C,m_tau,type,rho(x)))*sqrt(det(m_tau'*m_tau))*x(1);
-
-n=10;
-I=Gauss_Quadrature(40,g)
-rel_error_1=zeros(n,1);value=zeros(n,1);step_error=zeros(n-1,1);
-for k=1:n
-    value(k)=Gauss_Quadrature(k,g);
-    rel_error_1(k)=abs(I-value(k));
-    if k>1
-        step_error(k-1)=abs(value(k)-value(k-1));
-    end
-end
-rel_error_1
-step_error
-value
-
-fig = figure();
-
-semilogy(1:n,rel_error_1,'blue*',1:n-1, step_error,'redo')
-legend('relative error','step error')
-xlabel('n');
-grid on
-
 
 
